73fcee18f5cf4a161b7c71f8222b15eae3243ace
angelshau <shauangel2000@gmail.com>
2026-03-24 01:06:48 -0400

    fix

--- nrf_loader.c
+++ nrf_loader.c
@@ -26,7 +26,7 @@ static int handle_tok(void *ctx, void *data, size_t data_sz)
 {
     const struct event *e = data;
     printf("[%s: %s] pid=%u tid=%u\n",e->nf, e->api, e->pid, e->tid);
-    printf("arg1=%llx arg2=%llx arg3=%llx arg4=%llx\n", e->arg1, e->arg2, e->arg3, e->arg4)
+    printf("arg1=%llx arg2=%llx arg3=%llx arg4=%llx\n", e->arg1, e->arg2, e->arg3, e->arg4);
     return 0;
 }
 
