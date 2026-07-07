434bbb8369ac96841a4bb20d063d43a670199b58
angelshau <shauangel2000@gmail.com>
2026-07-07 02:30:58 -0400

    test

--- amf/xdp_ngap.c
+++ amf/xdp_ngap.c
@@ -1,8 +1,5 @@
-#include <linux/bpf.h>
+#include "../vmlinux.h"
 #include <bpf/bpf_helpers.h>
-#include <linux/if_ether.h>
-#include <linux/ip.h>
-#include <linux/sctp.h>
 #include <bpf/bpf_endian.h>
 
 char LICENSE[] SEC("license") = "GPL";
