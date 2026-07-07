ecfd25299d05a22fd09ab5c8329741b0a20ceb42
angelshau <shauangel2000@gmail.com>
2026-07-07 04:04:54 -0400

    test

--- amf/xdp_ngap.c
+++ amf/xdp_ngap.c
@@ -23,9 +23,12 @@ int xdp_prog(struct xdp_md *ctx)
     if ((void *)(eth + 1) > data_end)
         return XDP_PASS;
 
-    bpf_printk("XDP ifindex=%d eth proto=0x%x",
+    struct iphdr *iph = (void *)(eth + 1);
+
+    bpf_printk("XDP ifindex=%d eth proto=0x%x IP proto=%d",
                ctx->ingress_ifindex,
-               bpf_ntohs(eth->h_proto));
+               bpf_ntohs(eth->h_proto),
+               iph->protocol);
 
     return XDP_PASS;
 }
