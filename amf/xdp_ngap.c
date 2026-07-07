22308cfd7bf5a15e26063dcf336168c49d9f7977
angelshau <shauangel2000@gmail.com>
2026-07-07 03:56:45 -0400

    test eth

--- amf/xdp_ngap.c
+++ amf/xdp_ngap.c
@@ -14,35 +14,18 @@
 char LICENSE[] SEC("license") = "GPL";
 
 SEC("xdp")
-int xdp_ngap(struct xdp_md *ctx)
+int xdp_prog(struct xdp_md *ctx)
 {
     void *data = (void *)(long)ctx->data;
     void *data_end = (void *)(long)ctx->data_end;
 
     struct ethhdr *eth = data;
-    // if ((void *)(eth + 1) > data_end) return XDP_PASS;
+    if ((void *)(eth + 1) > data_end)
+        return XDP_PASS;
 
-    // bpf_printk("XDP eth proto=0x%x\n", bpf_ntohs(eth->h_proto));
-
-    // if (eth->h_proto != bpf_htons(ETH_P_IP)) return XDP_PASS;
-
-    struct iphdr *ip = (void *)(eth + 1);
-    // if ((void *)(ip + 1) > data_end) return XDP_PASS;
-
-    bpf_printk("XDP eth proto=0x%x IP proto=%d src=%x dst=%x\n", bpf_ntohs(eth->h_proto), ip->protocol, ip->saddr, ip->daddr);
-
-    if (ip->protocol != IPPROTO_SCTP) return XDP_PASS;
-
-    __u32 ihl = ip->ihl * 4;
-    if (ihl < 20) return XDP_PASS;
-
-    struct sctphdr *sctp = (void *)ip + ihl;
-    if ((void *)(sctp + 1) > data_end) return XDP_PASS;
-
-    bpf_printk("SCTP sport=%d dport=%d\n",
-               bpf_ntohs(sctp->source),
-               bpf_ntohs(sctp->dest));
-
+    bpf_printk("XDP ifindex=%d eth proto=0x%x",
+               ctx->ingress_ifindex,
+               bpf_ntohs(eth->h_proto));
 
     return XDP_PASS;
 }
