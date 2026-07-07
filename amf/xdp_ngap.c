762ee07493ebc4c7f1ae8e0c384f2ff4dfe93b9c
angelshau <shauangel2000@gmail.com>
2026-07-07 02:41:29 -0400

    test attach

--- amf/xdp_ngap.c
+++ amf/xdp_ngap.c
@@ -16,30 +16,31 @@ char LICENSE[] SEC("license") = "GPL";
 SEC("xdp")
 int xdp_ngap(struct xdp_md *ctx)
 {
-    void *data = (void *)(long)ctx->data;
-    void *data_end = (void *)(long)ctx->data_end;
+    // void *data = (void *)(long)ctx->data;
+    // void *data_end = (void *)(long)ctx->data_end;
 
-    struct ethhdr *eth = data;
-    if ((void *)(eth + 1) > data_end)
-        return XDP_PASS;
+    // struct ethhdr *eth = data;
+    // if ((void *)(eth + 1) > data_end)
+    //     return XDP_PASS;
 
-    if (eth->h_proto != bpf_htons(ETH_P_IP))
-        return XDP_PASS;
+    // if (eth->h_proto != bpf_htons(ETH_P_IP))
+    //     return XDP_PASS;
 
-    struct iphdr *ip = (void *)(eth + 1);
-    if ((void *)(ip + 1) > data_end)
-        return XDP_PASS;
+    // struct iphdr *ip = (void *)(eth + 1);
+    // if ((void *)(ip + 1) > data_end)
+    //     return XDP_PASS;
 
-    if (ip->protocol != IPPROTO_SCTP)
-        return XDP_PASS;
+    // if (ip->protocol != IPPROTO_SCTP)
+    //     return XDP_PASS;
 
-    struct sctphdr *sctp = (void *)ip + ip->ihl * 4;
-    if ((void *)(sctp + 1) > data_end)
-        return XDP_PASS;
+    // struct sctphdr *sctp = (void *)ip + ip->ihl * 4;
+    // if ((void *)(sctp + 1) > data_end)
+    //     return XDP_PASS;
 
-    if (bpf_ntohs(sctp->dest) == 38412) {
-        bpf_printk("AMF NGAP SCTP packet dst=38412\n");
-    }
+    // if (bpf_ntohs(sctp->dest) == 38412) {
+    //     bpf_printk("AMF NGAP SCTP packet dst=38412\n");
+    // }
+    bpf_printk("XDP_HIT\n");
 
     return XDP_PASS;
 }
