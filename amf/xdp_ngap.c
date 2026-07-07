7113b731b2b90d332b4e74aa841dafd42f9af5c8
angelshau <shauangel2000@gmail.com>
2026-07-07 05:03:41 -0400

    test ppid

--- amf/xdp_ngap.c
+++ amf/xdp_ngap.c
@@ -2,16 +2,21 @@
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_endian.h>
 
-#ifndef ETH_P_IP
 #define ETH_P_IP 0x0800
-#endif
-
-#ifndef IPPROTO_SCTP
 #define IPPROTO_SCTP 132
-#endif
-
+#define SCTP_PPID_NGAP 60
 
 char LICENSE[] SEC("license") = "GPL";
+
+static __always_inline void dump_bytes(void *base, void *data_end) {
+    for (int i = 0; i < 32; i++) {
+        if (base + i + 1 > data_end)
+            break;
+
+        __u8 b = *(__u8 *)(base + i);
+        bpf_printk("ngap[%d]=0x%x", i, b);
+    }
+}
 
 SEC("xdp")
 int xdp_prog(struct xdp_md *ctx)
@@ -27,14 +32,49 @@ int xdp_prog(struct xdp_md *ctx)
     if ((void *)(iph + 1) > data_end)
     return XDP_PASS;
 
-    if (iph->protocol == IPPROTO_SCTP)
-    {
-        bpf_printk("XDP eth proto=0x%x IP proto=%d src=%x dst=%x\n",
-                   bpf_ntohs(eth->h_proto),
-                   iph->protocol,
-                   iph->saddr,
-                   iph->daddr);
-    }
+    // Find SCTP packets
+    // if (iph->protocol == IPPROTO_SCTP)
+    // {
+    //     bpf_printk("XDP eth proto=0x%x IP proto=%d src=%x dst=%x\n",
+    //                bpf_ntohs(eth->h_proto),
+    //                iph->protocol,
+    //                iph->saddr,
+    //                iph->daddr);
+    // }
 
+    if (iph->protocol != IPPROTO_SCTP)
+        return XDP_PASS;
+
+    // IP header length may be > 20 bytes
+    void *sctp_ptr = (void *)iph + (iph->ihl * 4);
+
+    struct sctphdr_simple *sctp = sctp_ptr;
+    if ((void *)(sctp + 1) > data_end)
+        return XDP_PASS;
+
+    struct sctp_data_chunk *chunk = (void *)(sctp + 1);
+    if ((void *)(chunk + 1) > data_end)
+        return XDP_PASS;
+
+    __u8 chunk_type = chunk->type;
+    __u16 chunk_len = bpf_ntohs(chunk->length);
+    __u32 ppid = bpf_ntohl(chunk->ppid);
+
+    bpf_printk("SCTP sport=%d dport=%d chunk=%d len=%d ppid=%d",
+            bpf_ntohs(sctp->source),
+            bpf_ntohs(sctp->dest),
+            chunk_type,
+            chunk_len,
+            ppid);
+
+    // if (chunk_type != SCTP_DATA)
+    //     return XDP_PASS;
+
+    // if (ppid != SCTP_PPID_NGAP)
+    //     return XDP_PASS;
+
+    // void *ngap = (void *)(chunk + 1);
+    // if (ngap + 8 > data_end)
+    //     return XDP_PASS;
     return XDP_PASS;
 }
